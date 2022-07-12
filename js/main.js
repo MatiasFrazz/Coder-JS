1ca2d1300a7f0af68a86ef08fd5184e44aabbc6a
Mati Frazzetto <matiasfrazzetto60@gmail.com>
2022-07-12 14:22:50 -0300

    entrega-01

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-let nombre= prompt("Ingrese su nombre");
- alert("Hola " + nombre + ".");
+let nombre = prompt("Ingrese su nombre y apellido") 
 
-let 
+function hola(){}
+    hola (alert("Hola " + nombre + ".") )
+    alert("Ahora te vamos a pedir una por una tus calificaciones trimestrales")
+
+
+let nota1= parseInt ( prompt ("1er. Ingrese su calificacion") )
+let nota2= parseInt ( prompt ("2do. Ingrese su calificacion") )
+let nota3= parseInt ( prompt ("3er. Ingrese su calificacion") )
+let notaFinal = 0
+
+function sumar ( nota1, nota2, nota3) {
+    notaFinal = nota1 + nota2 + nota3
+} 
+
+
+
+function final (){
+    parseInt ( alert("Su nota final es: " + notaFinal) )
+}
+
+sumar(nota1, nota2, nota3)
+final(notaFinal)
+
+if(notaFinal <= 20){
+    alert("Su calificacion anual esta: Desaprobada")
+} else {
+    alert("Su calificacion anual esta: Aprobada")
+}
+
