abeef5bf6e1e9ca2477dfecf116da90fb10675f6
Mati Frazzetto <matiasfrazzetto60@gmail.com>
2022-07-21 18:10:49 -0300

    entrega Eventos

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-let nombre = prompt("Ingrese su nombre") 
-let apellido = prompt("Ingrese su apellido") 
+// let nombre = prompt("Ingrese su nombre")
+// let apellido = prompt("Ingrese su apellido")
 
-function hola(){}
-    hola (alert("Hola " + nombre + " " + apellido + ".") )
-    alert("Ahora te vamos a pedir una por una tus calificaciones trimestrales")
-
-
-let nota1= parseInt ( prompt ("1er. Ingrese su calificacion") )
-let nota2= parseInt ( prompt ("2do. Ingrese su calificacion") )
-let nota3= parseInt ( prompt ("3er. Ingrese su calificacion") )
-let notaFinal = 0
-
-function sumar ( nota1, nota2, nota3) {
-    notaFinal = (nota1 + nota2 + nota3) / 3
-} 
-
-function final (){
-    parseInt ( alert("Su nota final es: " + notaFinal) )
-}
-
-sumar(nota1, nota2, nota3)
-final(notaFinal)
-
-if(notaFinal >= 7){
-    alert("Su calificacion anual esta: Aprobada")
-} else {
-    alert("Su calificacion anual esta: Desaprobada")
-}
+// function hola(){}
+//     hola (alert("Hola " + nombre + " " + apellido + ".") )
+//     alert("Ahora te vamos a pedir una por una tus calificaciones trimestrales")
 
 
-let bienvenida = document.createElement("h1")
-bienvenida.setAttribute("class", "headerIndex")
-bienvenida.innerText = ("Bienvenidos a nuestra clase")
-document.getElementById("hIndex").appendChild(bienvenida)
+// let nota1= parseInt ( prompt ("1er. Ingrese su calificacion") )
+// let nota2= parseInt ( prompt ("2do. Ingrese su calificacion") )
+// let nota3= parseInt ( prompt ("3er. Ingrese su calificacion") )
+// let notaFinal = 0
+
+// function sumar ( nota1, nota2, nota3) {
+//     notaFinal = (nota1 + nota2 + nota3) / 3
+// }
+
+// function final (){
+//     parseInt ( alert("Su nota final es: " + notaFinal) )
+// }
+
+// sumar(nota1, nota2, nota3)
+// final(notaFinal)
+
+// if(notaFinal >= 7){
+//     alert("Su calificacion anual esta: Aprobada")
+// } else {
+//     alert("Su calificacion anual esta: Desaprobada")
+// }
+
+
+let bienvenida = document.createElement("h1");
+bienvenida.setAttribute("class", "headerIndex");
+bienvenida.innerText = ("Bienvenidos a nuestra clase");
+document.getElementById("hIndex").appendChild(bienvenida);
 
 class Alumno {
     constructor(nombre, apellido, materia, notaFinal) {
@@ -49,15 +49,19 @@ const infoAlumnos = ()=>{
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let materia = document.getElementById("materia").value;
-    let nota = parseInt(document.getElementById("notaFinal").value);
+    let notaFinal = document.getElementById("notaFinal").value;
 
     let agregarAlumno = new Alumno (nombre, apellido, materia, notaFinal);
     listaAlumnos.push(agregarAlumno);
     return agregarAlumno;
 }
 
+let formulario = document.getElementById("formAlumno")
+formulario.addEventListener("submit", formularioCompleto)
 
-
-
+function formularioCompleto(e){
+    e.preventDefault();
+    console.log(infoAlumnos())
+}
 
 
