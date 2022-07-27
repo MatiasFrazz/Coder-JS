bb18926b48e786c0aaee1691d054a3d14d7eb5d8
Mati Frazzetto <matiasfrazzetto60@gmail.com>
2022-07-27 17:32:37 -0300

    preentrega 2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 
-let bienvenida = document.createElement("h1");
-bienvenida.setAttribute("class", "headerIndex");
-bienvenida.innerText = ("Bienvenidos a nuestra clase");
-document.getElementById("hIndex").appendChild(bienvenida);
-
-class Alumno {
-    constructor(nombre, apellido, materia, notaFinal) {
+class Cliente {
+    constructor(nombre, localidad, codigoPostal) {
         this.nombre = nombre;
-        this.apellido = apellido;
-        this.materia = materia;
-        this.nota = notaFinal;
+        this.localidad = localidad;
+        this.codigoPostal = codigoPostal;
     }
 }
 
-let listaAlumnos = [];
+let infoClientes = [];
 
-const infoAlumnos = ()=>{
+const datosCliente = ()=>{
     let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    let materia = document.getElementById("materia").value;
-    let notaFinal = document.getElementById("notaFinal").value;
+    let localidad = document.getElementById("localidad").value;
+    let codigoPostal = document.getElementById("codigoPostal").value;
 
-    let agregarAlumno = new Alumno (nombre, apellido, materia, notaFinal);
-    listaAlumnos.push(agregarAlumno);
-    return agregarAlumno;
+    let clienteNuevo = new Cliente (nombre, localidad, codigoPostal);
+
+    if (localStorage.getItem("clientes") == null) {
+        infoClientes.push(clienteNuevo);
+        localStorage.setItem("clientes", JSON.stringify(infoClientes));
+    } else{
+        
+        const clienteRegistrado = JSON.parse(localStorage.getItem("clientes"));
+        clienteRegistrado.push(clienteNuevo);
+        localStorage.setItem("clientes", JSON.stringify(clienteRegistrado));
+        return clienteNuevo;  
+
+    }
 }
 
-let formulario = document.getElementById("formAlumno")
+let formulario = document.getElementById("formCliente")
 formulario.addEventListener("submit", formularioCompleto)
 
 function formularioCompleto(e){
     e.preventDefault();
-    console.log(infoAlumnos())
+    console.log(datosCliente())
 }
 
 
